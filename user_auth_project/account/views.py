5bc1b965890b75f998f6f8c31c6c6dba07b88140
ster <ster@localhost.localdomain>
2020-02-23 15:59:46 +0100

    Added navbar and login-logout activity

--- user_auth_project/account/views.py
+++ user_auth_project/account/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+def index(request):
+    return render(request, 'account/index.html', {})
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ('You successfully logged in'))
+            return redirect('index')
+        else:
+            messages.error(request, ('Error Logging in - Please try again'))
+            return redirect('login')
+    else:
+        return render(request, 'account/login.html', {})
+
+def Logout_user(request):
+    logout(request)
+    messages.success(request, "You have been Logout successfully")
+    return redirect('index')
